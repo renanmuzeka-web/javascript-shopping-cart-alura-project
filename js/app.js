3d1179caf5014d030bd74975f36b1a8482145aff
Renan Muzeka do Nascimento <renanmuzeka@gmail.com>
2026-02-06 19:13:34 -0300

    code done

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let totalGeral;
+limpar();
+
+function adicionar() {
+    //recuperar valores: nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    //calcular o preço, o subtotal
+    let preco = quantidade * valorUnitario;
+    //adicionar o produto no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`
+    //atualizar o valor total da compra
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+}
+
+function limpar() {
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+    totalGeral = 0;
+}
